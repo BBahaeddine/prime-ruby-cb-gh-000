68b023e775559754675268ee9cce448e20dd6f5b
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 09:31:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 def prime?(number)
   i = number - 1
   is_prime = true
-  loop do
+  while i > 1 do
     if (number % i) == 0
       is_prime = false
     end
     i -= 1
-    if i > 1
-      break
-    end
+    # if i > 1
+    #   break
+    # end
   end
   return is_prime
 end
