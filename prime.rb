5bf858397a8b546af2d5529c22d59567625d0947
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 08:19:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  isPrime = true
+  i = 2
+  loop do
+    if (number % i) == 0
+      isPrime = false
+    end
+    i += 1
+    if i == number
+      break
+    end
+  end
+  return isPrime
+end
